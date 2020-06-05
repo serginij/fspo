b2fcce02456e3cc2c535b340ce9c63032cafc081
serginij <kotlizkiy@gmail.com>
2020-06-05 18:18:30 +0300

    refactor: remove ArrayList with data

--- demo/src/EditLicence.java
+++ demo/src/EditLicence.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.sql.SQLException;
 
 /*  Разработал: Сергей Котлицкий
     github: https://github.com/serginij
@@ -22,7 +22,7 @@ public class EditLicence extends JFrame{
     private JTextField expiredDateField;
     private JPanel window;
 
-    EditLicence(ArrayList<Licence> licences, DefaultTableModel tableModel) {
+    EditLicence(DefaultTableModel tableModel) {
         setContentPane(window);
         setPreferredSize(new Dimension(500, 400));
         setTitle("Создание ВУ");
@@ -79,7 +79,6 @@ public class EditLicence extends JFrame{
                             licence.setExpiredDate(expiredDate);
 
                             // Добавляем данные в массив с данными
-                            licences.add(licence);
                             tableModel.addRow(new String[]{licence.getFullName(), licence.getNumber(), licence.getExpiredDate().toString()});
 
                             System.out.println(id);
@@ -101,90 +100,102 @@ public class EditLicence extends JFrame{
         });
     }
 
-    EditLicence(ArrayList<Licence> licences, DefaultTableModel tableModel, int index) {
+    EditLicence(DefaultTableModel tableModel, int index) {
         setContentPane(window);
         setPreferredSize(new Dimension(500, 400));
         setTitle("Редактирвоание ВУ");
 
-        // Получаем ВУ из массива данных
-        Licence licence = licences.get(index);
+        try {
+            // Получаем ВУ из массива данных
+            Licence licence;
+            ResultSet row = DB.select("select * from driver_licence where id="+ tableModel.getValueAt(index, 0));
+            System.out.println(tableModel.getValueAt(index, 0));
+            row.next();
+            licence = new Licence(row.getInt("id"),
+                    row.getString("name"),
+                    row.getString("surname"),
+                    row.getString("father_name"),
+                    row.getString("number"),
+                    row.getString("birthday"),
+                    row.getString("expired_date"));
 
-        // Заполняем текстовые поля данными
-        nameField.setText(licence.getName());
-        surnameField.setText(licence.getSurname());
-        fatherNameField.setText(licence.getFatherName());
-        numberField.setText(licence.getNumber());
-        birthdayField.setText(licence.getBirthday().toString());
-        expiredDateField.setText(licence.getExpiredDate().toString());
+            // Заполняем текстовые поля данными
+            nameField.setText(licence.getName());
+            surnameField.setText(licence.getSurname());
+            fatherNameField.setText(licence.getFatherName());
+            numberField.setText(licence.getNumber());
+            birthdayField.setText(licence.getBirthday().toString());
+            expiredDateField.setText(licence.getExpiredDate().toString());
 
+            save.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    String name = nameField.getText(), surname = surnameField.getText(), fatherName = fatherNameField.getText();
+                    String number = numberField.getText();
+                    String birthday = birthdayField.getText(), expiredDate = expiredDateField.getText();
 
-        save.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                String name = nameField.getText(), surname = surnameField.getText(), fatherName = fatherNameField.getText();
-                String number = numberField.getText();
-                String birthday = birthdayField.getText(), expiredDate = expiredDateField.getText();
+                    // Проверка на пустые поля
+                    if(name.trim().equals("") ||
+                            surname.trim().equals("") ||
+                            fatherName.trim().equals("") ||
+                            number.trim().equals("") ||
+                            birthday.trim().equals("") ||
+                            expiredDate.trim().equals("")) {
 
-                // Проверка на пустые поля
-                if(name.trim().equals("") ||
-                        surname.trim().equals("") ||
-                        fatherName.trim().equals("") ||
-                        number.trim().equals("") ||
-                        birthday.trim().equals("") ||
-                        expiredDate.trim().equals("")) {
-
-                    JOptionPane.showMessageDialog(null, "Поля для ввода не должны быть пустыми", "Внимание", JOptionPane.WARNING_MESSAGE);
-                } else {
-                    // Проверка на корректность ввода данных
-                    if(!Helpers.checkText(name) ||
-                            !Helpers.checkText(surname) ||
-                            !Helpers.checkText(fatherName) ||
-                            !Helpers.checkDate(birthday) ||
-                            !Helpers.checkDate(expiredDate) ||
-                            !Helpers.checkLengthStrict(number, 8)
-                    ) {
-                        JOptionPane.showMessageDialog(null, "Данные введены в некорректном формате", "Внимание", JOptionPane.WARNING_MESSAGE);
+                        JOptionPane.showMessageDialog(null, "Поля для ввода не должны быть пустыми", "Внимание", JOptionPane.WARNING_MESSAGE);
                     } else {
-                        try {
-                            // Строка с запросом на редактирование
-                            String query = "update driver_licence set name='" + name +
-                                    "', surname='" + surname +
-                                    "', father_name='" +fatherName +
-                                    "', number='" + number +
-                                    "', birthday='"+birthday+
-                                    "', expired_date='" +expiredDate +
-                                    "' where id=" + licence.getId();
+                        // Проверка на корректность ввода данных
+                        if(!Helpers.checkText(name) ||
+                                !Helpers.checkText(surname) ||
+                                !Helpers.checkText(fatherName) ||
+                                !Helpers.checkDate(birthday) ||
+                                !Helpers.checkDate(expiredDate) ||
+                                !Helpers.checkLengthStrict(number, 8)
+                        ) {
+                            JOptionPane.showMessageDialog(null, "Данные введены в некорректном формате", "Внимание", JOptionPane.WARNING_MESSAGE);
+                        } else {
+                            try {
+                                // Строка с запросом на редактирование
+                                String query = "update driver_licence set name='" + name +
+                                        "', surname='" + surname +
+                                        "', father_name='" +fatherName +
+                                        "', number='" + number +
+                                        "', birthday='"+birthday+
+                                        "', expired_date='" +expiredDate +
+                                        "' where id=" + licence.getId();
 
-                            DB.update(query);
+                                DB.update(query);
 
-                            // Заполняем поля класса обновленными значениями
-                            licence.setName(name);
-                            licence.setSurname(surname);
-                            licence.setFatherName(fatherName);
-                            licence.setNumber(number);
-                            licence.setBirthday(birthday);
-                            licence.setExpiredDate(expiredDate);
+                                // Заполняем поля класса обновленными значениями
+                                licence.setName(name);
+                                licence.setSurname(surname);
+                                licence.setFatherName(fatherName);
+                                licence.setNumber(number);
+                                licence.setBirthday(birthday);
+                                licence.setExpiredDate(expiredDate);
 
-                            // Обновляем массив с данными и модель таблицы
-                            licences.set(index, licence);
-                            tableModel.setValueAt(licence.getFullName(), index, 0);
-                            tableModel.setValueAt(licence.getNumber(), index, 1);
-                            tableModel.setValueAt(licence.getExpiredDate(), index, 2);
-                        } catch (Exception ex) {
-                            System.out.println(ex);
-                            JOptionPane.showMessageDialog(null, "Произошла ошибка при сохранении в бд", "Ошибка", JOptionPane.ERROR_MESSAGE);
+                                // Обновляем массив с данными и модель таблицы
+                                tableModel.setValueAt(licence.getFullName(), index, 1);
+                                tableModel.setValueAt(licence.getNumber(), index, 2);
+                                tableModel.setValueAt(licence.getExpiredDate(), index, 3);
+                            } catch (Exception ex) {
+                                System.out.println(ex);
+                                JOptionPane.showMessageDialog(null, "Произошла ошибка при сохранении в бд", "Ошибка", JOptionPane.ERROR_MESSAGE);
+                            }
+                            dispose();
                         }
-                        dispose();
                     }
                 }
-            }
-        });
+            });
 
-        cancel.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                dispose();
-            }
-        });
-    }
+            cancel.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    dispose();
+                }
+            });
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            JOptionPane.showMessageDialog(null, "Произошла ошибка при обращении к бд", "Ошибка", JOptionPane.ERROR_MESSAGE);
+        }}
 }
